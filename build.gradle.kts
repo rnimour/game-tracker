a2873a24b5721807876875eae09a3baec1aaaccb
rnimour <rudenimour@hotmail.com>
2024-06-11 10:51:30 +0200

    upgrade to K2 compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.jpa") version "1.9.24" // for no-arg constructors
-	kotlin("plugin.spring") version "1.9.24" // for all-open @Configuration classes
+	kotlin("jvm") version "2.0.0"
+	kotlin("plugin.jpa") version "2.0.0" // for no-arg constructors
+	kotlin("plugin.spring") version "2.0.0" // for all-open @Configuration classes
 
 	idea
 }
