585dc86dfb67b7510a8db7b40be5a41b096afe88
rnimour <rudenimour@hotmail.com>
2024-07-04 13:24:28 +0200

    neater DTO

--- src/main/kotlin/com/rnimour/trials/games/GameDTO.kt
+++ src/main/kotlin/com/rnimour/trials/games/GameDTO.kt
@@ -3,48 +3,29 @@ package com.rnimour.trials.games
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class GameDTOCreateRequest(
-    @JsonProperty("name")
-    var name: String,
-    @JsonProperty("playStatus")
-    var playStatus: PlayStatus,
-    @JsonProperty("releaseYear")
-    var releaseYear: Int,
-    @JsonProperty("developer")
-    var developer: String,
-    @JsonProperty("series")
-    var series: String?,
-    @JsonProperty("genre")
-    var genre: String?,
+    @JsonProperty var name: String,
+    @JsonProperty var playStatus: PlayStatus,
+    @JsonProperty var releaseYear: Int,
+    @JsonProperty var developer: String,
+    @JsonProperty var series: String?,
+    @JsonProperty var genre: String?,
 )
 
 data class GameDTOUpdateRequest(
-    @JsonProperty("name")
-    var name: String? = null,
-    @JsonProperty("playStatus")
-    var playStatus: PlayStatus? = null,
-    @JsonProperty("releaseYear")
-    var releaseYear: Int? = null,
-    @JsonProperty("developer")
-    var developer: String? = null,
-    @JsonProperty("series")
-    var series: String? = null,
-    @JsonProperty("genre")
-    var genre: String? = null,
+    @JsonProperty var name: String? = null,
+    @JsonProperty var playStatus: PlayStatus? = null,
+    @JsonProperty var releaseYear: Int? = null,
+    @JsonProperty var developer: String? = null,
+    @JsonProperty var series: String? = null,
+    @JsonProperty var genre: String? = null,
 )
 
 data class GameDTOResponse(
-    @JsonProperty("id")
-    var id: Long?,
-    @JsonProperty("name")
-    var name: String?,
-    @JsonProperty("playStatus")
-    var playStatus: PlayStatus?,
-    @JsonProperty("releaseYear")
-    var releaseYear: Int?,
-    @JsonProperty("developer")
-    var developer: String?,
-    @JsonProperty("series")
-    var series: String?,
-    @JsonProperty("genre")
-    var genre: String?,
+    @JsonProperty var id: Long?,
+    @JsonProperty var name: String?,
+    @JsonProperty var playStatus: PlayStatus?,
+    @JsonProperty var releaseYear: Int?,
+    @JsonProperty var developer: String?,
+    @JsonProperty var series: String?,
+    @JsonProperty var genre: String?,
 )
